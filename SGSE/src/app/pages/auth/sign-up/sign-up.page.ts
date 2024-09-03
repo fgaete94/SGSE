fe5c31f2d5b996ea078c0b59151a95550661c46d
Felipe Gaete Troncoso <f.gaete@duocuc.cl>
2024-09-03 19:49:48 -0400

    solucionado error de sobre escritura en BBDD

--- SGSE/src/app/pages/auth/sign-up/sign-up.page.ts
+++ SGSE/src/app/pages/auth/sign-up/sign-up.page.ts
@@ -64,7 +64,7 @@ export class SignUpPage implements OnInit {
       const loading = await this.utilsSvc.loading();
       await loading.present();
 
-      let path = 'users/${uid}'
+      let path = `users/${uid}`
       delete this.form.value.password;
 
 
